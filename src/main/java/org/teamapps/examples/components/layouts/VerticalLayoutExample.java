e2b41a8720c4eb0b3bf304253d2ac93e202f6bbe
Benjamin Jesuiter <bjesuiter@gmail.com>
2019-06-25 22:05:57 +0200

    add example for VerticalLayout-flexProperties

--- src/main/java/org/teamapps/examples/components/layouts/VerticalLayoutExample.java
+++ src/main/java/org/teamapps/examples/components/layouts/VerticalLayoutExample.java
@@ -24,15 +24,34 @@ import org.teamapps.documentation.generator.annotation.TeamAppsDocClass;
 import org.teamapps.documentation.generator.annotation.TeamAppsDocMethod;
 import org.teamapps.icon.material.MaterialIcon;
 import org.teamapps.ux.component.Component;
+import org.teamapps.ux.component.absolutelayout.Length;
+import org.teamapps.ux.component.absolutelayout.SizeUnit;
 import org.teamapps.ux.component.field.Label;
+import org.teamapps.ux.component.flexcontainer.FlexSizeUnit;
+import org.teamapps.ux.component.flexcontainer.FlexSizingPolicy;
 import org.teamapps.ux.component.flexcontainer.VerticalLayout;
+import org.teamapps.ux.component.panel.Panel;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 @TeamAppsDocClass(title = "Vertical Layout")
 public class VerticalLayoutExample {
+    
+    private int MAX_WIDTH_INT = 400;
+    private int MAX_HEIGHT_INT = 200;
+    
+    private Panel generateContainerPanel(String title) {
+        Panel containerPanel = new Panel(MaterialIcon.LAYERS, title);
+        containerPanel.setStretchContent(false);
+        containerPanel.setPadding(10);
+        containerPanel.setMaxWidth(new Length(MAX_WIDTH_INT, SizeUnit.PIXEL));
+        containerPanel.setMaxHeight(new Length(MAX_HEIGHT_INT, SizeUnit.PIXEL));
+        
+        return containerPanel;
+    }
     
     /**
      * <code>VerticalLayout</code> allows multiple children to be stacked vertically.
@@ -42,7 +61,7 @@ public class VerticalLayoutExample {
     @TeamAppsDocMethod(title = "Simple Vertical Layout", images = "VerticalLayout-simple.png")
     public Component simpleVerticalLayout() {
         VerticalLayout layout = new VerticalLayout();
-
+        
         // This makes the vertical layout scrollable
         layout.setCssStyle("overflow-y", "auto");
         
@@ -52,6 +71,41 @@ public class VerticalLayoutExample {
             layout.addComponent(new Label(icon.getIconName(), icon));
         }
         
-        return layout;
+        // This panel is used to contain the vertical layout in a small box.
+        var containerPanel = this.generateContainerPanel("Simple Vertical Layout");
+        containerPanel.setContent(layout);
+        
+        return containerPanel;
     }
+    
+    
+    /**
+     * Since <code>VerticalLayout</code> is based on CSS FlexBox, each item can be sized with FlexSizingProperties.
+     */
+    @TeamAppsDocMethod(title = "Vertical Layout with FlexSizingProperties", images = "VerticalLayout-flexProperties.png")
+    public Component FlexSizingProperties() {
+        VerticalLayout layout = new VerticalLayout();
+        
+        // This makes the vertical layout scrollable
+        layout.setCssStyle("overflow-y", "auto");
+        
+        List<MaterialIcon> iconList = new ArrayList<>(Arrays.asList(MaterialIcon.values())).subList(0, 100);
+        
+        Random random = new Random();
+        
+        for (MaterialIcon icon : iconList) {
+            Label label = new Label(icon.getIconName(), icon);
+            float baseSize = 20 + random.nextInt(50);
+            layout.addComponent(label, new FlexSizingPolicy(baseSize, FlexSizeUnit.PIXEL, 1, 1)
+            );
+        }
+        
+        // This panel is used to contain the vertical layout in a small box.
+        var containerPanel = this.generateContainerPanel("Vertical Layout with FlexSizingProperties");
+        containerPanel.setContent(layout);
+        
+        return containerPanel;
+    }
+    
+    
 }
